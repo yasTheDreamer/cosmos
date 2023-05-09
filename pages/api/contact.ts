789d6feb4b145205084baf74fd9e725ed5005250
yasTheDreamer <yassine19265@gmail.com>
2023-05-09 00:47:15 +0000

    fix cors preflight options request

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,31 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { transporter, mailOptions } from '../../config/nodemailer'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { transporter, mailOptions } from "../../config/nodemailer";
 
 type Data = {
-  message: string
-}
+  message: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   if (req && req.body) {
-    let {name, email, message} = req.body
+    if (req.method == "OPTIONS") {
+      return res.status(200).json({ message: "preflight succeeded" });
+    }
+
+    let { name, email, message } = req.body;
 
     try {
       await transporter.sendMail({
         ...mailOptions,
         subject: "portfolio message",
-        html: `<h1>name: ${name}</h1><br/><h1>email : ${email}</h1><br/><h2>message : ${message}</h2>`
-      })
+        html: `<h1>name: ${name}</h1><br/><h1>email : ${email}</h1><br/><h2>message : ${message}</h2>`,
+      });
 
-      return res.status(200).json({message : "success"})
-
-    } catch (error: any ){
-      return res.status(400).json({ message: error.message })
+      return res.status(200).json({ message: "success" });
+    } catch (error: any) {
+      return res.status(400).json({ message: error.message });
     }
   }
 
-  return res.status(400).json({ message: 'Bad Request' })
+  return res.status(400).json({ message: "Bad Request" });
 }
--- services/contact.service.ts
+++ services/contact.service.ts
@@ -1,9 +1,13 @@
-import http from "../lib/handler"
+import http from "../lib/handler";
 
 class ContactService {
-    sendMail(data : Object): any {
-        return http.post("/contact", JSON.stringify(data))
-    }
+  sendMail(data: Object): any {
+    return http.post("/contact", JSON.stringify(data));
+  }
+
+  handlePreflight() {
+    return http.options("/contact");
+  }
 }
 
 export default new ContactService();
